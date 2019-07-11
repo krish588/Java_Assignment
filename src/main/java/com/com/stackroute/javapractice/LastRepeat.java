8d12b43228a781a3357530e2016f17edb6ecb21c
leelakrishna588 <leelakrishna588@gmail.com>
2019-07-11 15:38:02 +0530

    lastprogramcompleted

---
+++ src/main/java/com/com/stackroute/javapractice/LastRepeat.java
@@ -0,0 +1,21 @@
+package com.stackroute.javapractice;
+
+import java.util.Scanner;
+
+public class LastRepeat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.nextLine();
+        System.out.println("Enter number of repeats");
+        int number = sc.nextInt();
+        for (int i = 0;i<str.length()-number; i++) {
+            System.out.print(str.charAt(i));
+        }
+        for (int i = 0; i < number+1; i++) {
+            for (int j = str.length() - number; j < str.length(); j++) {
+                System.out.print(str.charAt(j));
+            }
+        }
+    }
+}
